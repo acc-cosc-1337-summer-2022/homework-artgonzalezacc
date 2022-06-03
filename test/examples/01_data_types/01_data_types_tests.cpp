25998ac6c6b60da292bbcf0f2fbff0ad06203115
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-06-03 03:23:46 +0000

    data types

--- test/examples/01_data_types/01_data_types_tests.cpp
+++ test/examples/01_data_types/01_data_types_tests.cpp
@@ -1,6 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "int.h"
+#include "decimals.h"
+#include "char.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -15,14 +17,28 @@ TEST_CASE("Test echo variable function")
 }
 
 /*test case add_to_double_1 with 0 as parameter*/
+TEST_CASE("Test add to double1 with 0 as parameter")
+{
+//	REQUIRE(add_to_double_1(0) == .9);
+}
 
 /*test case add_to_double_1 with 1 as parameter*/
+TEST_CASE("Test add to double1 with 1 as parameter")
+{
+//	REQUIRE(add_to_double_1(1) == 1.9);
+}
+
 
 /*test case add_to_double_2 with 0 as parameter*/
 
 /*test case add_to_double_2 with 1 as parameter*/
 
 /*test case char test ASCII values */
+TEST_CASE("Test that value A returns 65")
+{
+	REQUIRE(get_char_ascii_value('A') == 65);
+	REQUIRE(get_char_ascii_value('a') == 97);
+}
 
 /*test case string test ASCII values Joe characters are equal to 74, 111, and 101*/
 
