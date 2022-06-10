2f5c10c9d34509965426499b04a843a4e3c71f67
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-06-10 23:48:59 +0000

    conditional structures

--- src/examples/03_conditionals/04_switch/main.cpp
+++ src/examples/03_conditionals/04_switch/main.cpp
@@ -1,8 +1,12 @@
 #include<iostream>
+#include "switch.h"
+
+using std::cout;
 
 int main() 
 {
-	
+	int num = 10;
+	string result = menu(num);
 
 	return 0;
 }
--- test/examples/03_conditionals/03_conditionals_tests.cpp
+++ test/examples/03_conditionals/03_conditionals_tests.cpp
@@ -1,6 +1,44 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "if.h"
+#include "if_else.h"
+#include "if_else_if.h"
+#include "switch.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Verify is overtime function")
+{
+	REQUIRE(is_overtime(35) == false);
+	REQUIRE(is_overtime(40) == false);
+	REQUIRE(is_overtime(41) == true);
+}
+
+TEST_CASE("Verify is even function")
+{
+	REQUIRE(is_even(2) == true);
+	REQUIRE(is_even(3) == false);
+	REQUIRE(is_even(4) == true);
+}
+
+TEST_CASE("Verify get generation function")
+{
+	REQUIRE(get_generation(1996) == "Centenial");
+	REQUIRE(get_generation(1990) == "Millenial");
+	REQUIRE(get_generation(1966) == "Generation X");
+	REQUIRE(get_generation(1947) == "Baby Boomer");
+	REQUIRE(get_generation(1940) == "Silent Generation");
+	REQUIRE(get_generation(2025) == "Invalid Year");
+}
+
+TEST_CASE("Verify menu function")
+{
+	REQUIRE(menu(0) == "Invalid Option");
+	REQUIRE(menu(1) == "Option 1");
+	REQUIRE(menu(2) == "Option 2");
+	REQUIRE(menu(3) == "Option 3");
+	REQUIRE(menu(4) == "Option 4");
+	REQUIRE(menu(5) == "Invalid Option");
 }
