01327a75caa0b468546f6accdaa343b90daf4558
Art Gonzalez <arturo.gonzalez@austincc.edu>
2022-07-01 00:34:46 +0000

    references and pointers

--- src/examples/11_memory/02_dynamic_memory/main.cpp
+++ src/examples/11_memory/02_dynamic_memory/main.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+
+using std::cout;
+
 int main() 
 {
+	int* num = new int(5);//create dynamic memory 
+	cout<<*num<<"\n";//use memory
+	delete num; //delete memory
+	num = 0;
+
 	
 	return 0;
 }
--- test/examples/11_memory/11_memory_tests.cpp
+++ test/examples/11_memory/11_memory_tests.cpp
@@ -1,6 +1,16 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "ref_pointers.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Test function reference and pointer parameter")
+{
+	int num = 5, num1 = 15;
+	ref_ptr(num, &num1);
+
+	REQUIRE(num == 100);//???
+	REQUIRE(num1 == 200);//???
 }
